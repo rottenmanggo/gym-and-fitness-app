75bd448fb71afe1ffb99218981f951e5f18f50d7
arivernando21 <arivernando781@gmail.com>
2026-05-04 20:17:44 +0700

    setup database connection and update payments schema

--- src/config/Database.java
+++ src/config/Database.java
@@ -1,5 +1,23 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Database {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/gymbrut";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Koneksi database berhasil!");
+            return conn;
+        } catch (Exception e) {
+            System.out.println("Koneksi database gagal!");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
