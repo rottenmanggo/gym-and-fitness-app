2c9a4897238787be42b9648d3f7e5bacbeb1fdc6
yels06 <dlsrgh@gmail.com>
2026-05-05 17:15:52 +0700

    idk

--- src/config/Database.java
+++ src/config/Database.java
@@ -5,4 +5,20 @@ import java.sql.DriverManager;
 
 public class Database {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/db_gymbrut";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+<<<<<<< HEAD
+=======
+            System.out.println("Koneksi database gagal!");
+>>>>>>> 7c2aef1530dd76ffab9d85aa76448403dd75423b
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
