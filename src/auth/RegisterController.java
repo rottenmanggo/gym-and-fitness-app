6d54245dd5b583575b6239cf2c419918fa897fa7
makraev06 <sonybae34@gmail.com>
2026-05-06 23:10:36 +0700

    fix : enter login and register

--- src/auth/RegisterController.java
+++ src/auth/RegisterController.java
@@ -2,6 +2,7 @@ package auth;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -17,10 +18,71 @@ public class RegisterController {
     private TextField phoneField;
     @FXML
     private PasswordField passwordField;
+
+    @FXML
+    private ComboBox<String> genderBox;
+    @FXML
+    private TextField ageField;
+    @FXML
+    private TextField heightField;
+    @FXML
+    private TextField weightField;
+    @FXML
+    private TextField targetField;
+
     @FXML
     private Label alertLabel;
 
     private final AuthService authService = new AuthService();
+
+    @FXML
+    public void initialize() {
+
+        genderBox.getItems().addAll(
+                "Laki-laki",
+                "Perempuan");
+    }
+
+    @FXML
+    private void focusEmail() {
+        emailField.requestFocus();
+    }
+
+    @FXML
+    private void focusPassword() {
+        passwordField.requestFocus();
+    }
+
+    @FXML
+    private void focusPhone() {
+        phoneField.requestFocus();
+    }
+
+    @FXML
+    private void focusGender() {
+        genderBox.requestFocus();
+        genderBox.show();
+    }
+
+    @FXML
+    private void focusAge() {
+        ageField.requestFocus();
+    }
+
+    @FXML
+    private void focusHeight() {
+        heightField.requestFocus();
+    }
+
+    @FXML
+    private void focusWeight() {
+        weightField.requestFocus();
+    }
+
+    @FXML
+    private void focusTarget() {
+        targetField.requestFocus();
+    }
 
     @FXML
     private void handleRegister() {
@@ -54,16 +116,19 @@ public class RegisterController {
         try {
             System.out.println("Go to Login clicked");
             Node nodeToUse = emailField != null ? emailField : alertLabel;
+
             if (nodeToUse == null) {
                 System.err.println("No valid node found for scene change");
                 return;
             }
+
             SceneManager.changeScene(
                     nodeToUse,
                     "/auth/Login.fxml",
                     "Login",
                     1100,
                     720);
+
             System.out.println("Login scene change initiated");
         } catch (Exception e) {
             e.printStackTrace();
